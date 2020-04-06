d0e69fbc2c847ed6d404c578abc303579727af6e
Conner Vieira <vieirast@icloud.com>
2020-04-06 09:12:57 -0400

    Initial Commit

---
+++ Circulous/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Circulous
+//
+//  Created by Conner Vieira on 4/6/20.
+//  Copyright © 2020 V0LT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Circulous/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Circulous
+//
+//  Created by Conner Vieira on 4/6/20.
+//  Copyright © 2020 V0LT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
